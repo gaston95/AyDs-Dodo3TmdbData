0d8a1f0f77d180de45c52d019d3edc8c9f03fafa
tscandalitta <tscandalitta@gmail.com>
2020-05-13 16:47:15 -0300

    Retornamos el overview solo si hay resultado

--- src/main/java/ayds/dodo/movieinfo/moredetails/fulllogic/DataBase.kt
+++ src/main/java/ayds/dodo/movieinfo/moredetails/fulllogic/DataBase.kt
@@ -82,10 +82,12 @@ object DataBase {
             val statement = connection.createStatement()
             statement.queryTimeout = 30 // set timeout to 30 sec.
             val rs = statement.executeQuery("select * from info WHERE title = '$title'")
-            rs.next()
-            return rs.getString("plot")
+            if(!rs.isClosed) {
+                rs.next()
+                return rs.getString("plot")
+            }
         } catch (e: SQLException) { // if the error message is "out of memory",
-// it probably means no database file is found
+            // it probably means no database file is found
             System.err.println("Get title error " + e.message)
         } finally {
             try {
