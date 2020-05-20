51424a66442d1db79b62036fcc60dd8ccfb33394
tscandalitta <tscandalitta@gmail.com>
2020-05-19 21:53:57 -0300

    Revert "Solucionamos las nullPointerException cuando el JSON de los datos de la pelicula contiene campos nulos"
    This reverts commit 22aab1f7

--- src/main/java/ayds/dodo/movieinfo/moredetails/fulllogic/OtherInfoWindow.java
+++ src/main/java/ayds/dodo/movieinfo/moredetails/fulllogic/OtherInfoWindow.java
@@ -71,47 +71,37 @@ public class OtherInfoWindow {
 
             JsonObject result = null;
 
-            boolean movieFound = false;
-
             while (resultIterator.hasNext()) {
               result = resultIterator.next().getAsJsonObject();
 
-              JsonElement yearJson = result.get("release_date");
-              String year = yearJson == null ? "" : yearJson.getAsString().split("-")[0];
+              String year = result.get("release_date").getAsString().split("-")[0];
 
-              if (year.equals(movie.getYear())) {
-                movieFound = true;
-                break;
-              }
+              if (year.equals(movie.getYear())) break;
             }
 
-            JsonElement extract = null;
-            JsonElement backdropPathJson = null;
-            JsonElement posterPath = null;
 
-            if(result != null && movieFound){
-              extract = result.get("overview");
-              backdropPathJson = result.get("backdrop_path");
-              posterPath = result.get("poster_path");
-            }
+            JsonElement extract = result.get("overview");
+
+            JsonElement backdropPathJson = result.get("backdrop_path");
 
             String backdropPath = null;
 
             System.out.println("backdropPathJson " + backdropPathJson);
 
-            if (backdropPathJson != null && !backdropPathJson.isJsonNull()) {
+            if (!backdropPathJson.isJsonNull()) {
               backdropPath =  backdropPathJson.getAsString();
             }
 
-            if (extract == null || extract.isJsonNull()) {
+
+            JsonElement posterPath = result.get("poster_path");
+
+            if (extract == null) {
               text = "No Results";
-              path = "https://www.themoviedb.org/assets/2/v4/logos/256x256-dark-bg-01a111196ed89d59b90c31440b0f77523e9d9a9acac04a7bac00c27c6ce511a9.png";
             } else {
               text = extract.getAsString().replace("\\n", "\n");
               text = textToHtml(text, movie.getTitle());
 
-              if(posterPath != null && !posterPath.isJsonNull())
-                text+="\n" + "<a href=https://image.tmdb.org/t/p/w400/" + posterPath.getAsString() +">View Movie Poster</a>";
+              text+="\n" + "<a href=https://image.tmdb.org/t/p/w400/" + posterPath.getAsString() +">View Movie Poster</a>";
 
               if(backdropPath != null)
                 path = "https://image.tmdb.org/t/p/w400/" + backdropPath;
