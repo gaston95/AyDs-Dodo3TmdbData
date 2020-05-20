50c89180601b1b7fdeba585dea69f855e58fe212
tscandalitta <tscandalitta@gmail.com>
2020-05-19 21:42:48 -0300

    Revert "Retornamos el imageUrl solo si hay resultado"
    This reverts commit f044baf8

--- src/main/java/ayds/dodo/movieinfo/moredetails/fulllogic/DataBase.kt
+++ src/main/java/ayds/dodo/movieinfo/moredetails/fulllogic/DataBase.kt
@@ -79,13 +79,11 @@ object DataBase {
             val statement = connection.createStatement()
             statement.queryTimeout = 30 // set timeout to 30 sec.
             val rs = statement.executeQuery("select * from info WHERE title = '$title'")
-            if(!rs.isClosed) {
-                rs.next()
-                return rs.getString("image_url")
-            }
+            rs.next()
+            return rs.getString("image_url")
         } catch (e: SQLException) { // if the error message is "out of memory",
-            // it probably means no database file is found
-            System.err.println("Get image error " + e.message)
+// it probably means no database file is found
+            System.err.println("Get title error " + e.message)
         } finally {
             try {
                 connection?.close()
