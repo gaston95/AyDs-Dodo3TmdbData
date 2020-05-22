650cc49866d206752efb47d552c1e48be4ca3551
tscandalitta <tscandalitta@gmail.com>
2020-05-22 18:34:55 -0300

    Eliminamos funcion testDB() que no era utilizada

--- src/main/java/ayds/dodo/movieinfo/moredetails/fulllogic/DataBase.kt
+++ src/main/java/ayds/dodo/movieinfo/moredetails/fulllogic/DataBase.kt
@@ -24,34 +24,6 @@ object DataBase {
         }
     }
 
-    fun testDB() {
-        var connection: Connection? = null
-        try { // create a database connection
-            connection = DriverManager.getConnection("jdbc:sqlite:./extra_info.db")
-            val statement = connection.createStatement()
-            statement.queryTimeout = 30 // set timeout to 30 sec.
-            //statement.executeUpdate("drop table if exists person");
-//statement.executeUpdate("create table person (id integer, name string)");
-//statement.executeUpdate("insert into person values(1, 'leo')");
-//statement.executeUpdate("insert into person values(2, 'yui')");
-            val rs = statement.executeQuery("select * from info")
-            while (rs.next()) { // read the result set
-                println("id = " + rs.getInt("id"))
-                println("title = " + rs.getString("title"))
-                println("source = " + rs.getString("source"))
-            }
-        } catch (e: SQLException) { // if the error message is "out of memory",
-// it probably means no database file is found
-            System.err.println(e.message)
-        } finally {
-            try {
-                connection?.close()
-            } catch (e: SQLException) { // connection close failed.
-                System.err.println(e)
-            }
-        }
-    }
-
     @JvmStatic
     fun saveMovieInfo(title: String, plot: String, imageUrl: String) {
         var connection: Connection? = null
