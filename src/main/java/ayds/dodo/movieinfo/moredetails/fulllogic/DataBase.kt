ecd04d402d20619fcdb9dcf385bcadc808551649
Manuel Enriquez Merino <manuel_em7@hotmail.com>
2020-05-27 18:06:44 -0300

    Reordenado el codigo Agregado metodo getMovieColumn Actualizados getOverview y getImageUrl

--- src/main/java/ayds/dodo/movieinfo/moredetails/fulllogic/DataBase.kt
+++ src/main/java/ayds/dodo/movieinfo/moredetails/fulllogic/DataBase.kt
@@ -18,85 +18,12 @@ object DataBase {
         }
 
     private const val extraInfoDBURL = "jdbc:sqlite:./extra_info.db"
-    private const val plot = "plot"
-    private const val imageUrl = "image_url"
+    private const val plotColumn = "plot"
+    private const val imageUrlColumn = "image_url"
     private const val createTableQuery = "create table if not exists info (id INTEGER PRIMARY KEY AUTOINCREMENT, title string, plot string, image_url string, source integer)"
     private const val singleQuote = "'"
     private const val doubleQuote = "''"
 
-    @JvmStatic
-    fun createNewDatabase() {
-        openConnectionToExtraInfo()
-        createInfoTableifNeeded()
-        closeConnectionToExtraInfo()
-    }
-
-    @JvmStatic
-    fun saveMovieInfo(title: String, plot: String, imageUrl: String) {
-        openConnectionToExtraInfo()
-        insertMovieInfo(title,plot,imageUrl)
-        closeConnectionToExtraInfo()
-    }
-
-    @JvmStatic
-    fun getOverview(title: String): String? {
-        try{
-            getConnectionToExtraInfo().use{
-                return it.initializeTitleResultSet(title).getPlot()
-            }
-        } catch (e: Exception){
-            System.err.println(e)
-        }
-        return null
-    }
-
-    @JvmStatic
-    fun getImageUrl(title: String): String? {
-        try{
-            getConnectionToExtraInfo().use{
-                return it.initializeTitleResultSet(title).getImageURL()
-            }
-        } catch (e: Exception){
-            System.err.println(e)
-        }
-        return null
-    }
-
-    private fun getConnectionToExtraInfo(): Connection = DriverManager.getConnection(extraInfoDBURL)
-
-    private fun Connection.initializeStatement(): Statement {
-        val statement = this.createStatement()
-        statement.queryTimeout = 30
-        return statement
-    }
-
-    private fun Connection.initializeTitleResultSet(title: String) =
-            this.initializeStatement().getTitleResultSet(title)
-
-    private fun Statement.getTitleResultSet(title: String): ResultSet =
-        this.executeQuery("select * from info WHERE title = '$title'")
-
-    private fun ResultSet.getPlot(): String? {
-        return if(!this.isClosed) {
-            this.next()
-            this.getString(plot)
-        } else
-            null
-    }
-
-    private fun ResultSet.getImageURL(): String? {
-        return if(!this.isClosed) {
-            this.next()
-            this.getString(imageUrl)
-        } else
-            null
-    }
-
-
-
-    private fun String.replaceQuotes() = this.replace(singleQuote, doubleQuote)
-
-    //---------------------------------------------------------------------
     private fun openConnectionToExtraInfo() {
         try {
             connection = DriverManager.getConnection(extraInfoDBURL)
@@ -113,18 +40,13 @@ object DataBase {
         }
     }
 
-    private fun isInfotableCreated(): Boolean {
+    private fun isInfoTableCreated(): Boolean {
         try {
             val tables = connection.metaData?.getTables(null, null, "info", null)
             return tables?.next() ?: false
         } catch (e: SQLException) {
         }
         return false
-    }
-
-    private fun createInfoTableifNeeded(){
-        if(!isInfotableCreated())
-            createInfoTable()
     }
 
     private fun createInfoTable(){
@@ -135,6 +57,27 @@ object DataBase {
         }
     }
 
+    private fun createInfoTableIfNeeded(){
+        if(!isInfoTableCreated())
+            createInfoTable()
+    }
+
+    @JvmStatic
+    fun createNewDatabase() {
+        openConnectionToExtraInfo()
+        createInfoTableIfNeeded()
+        closeConnectionToExtraInfo()
+    }
+
+    private fun String.replaceQuotes() = this.replace(singleQuote, doubleQuote)
+
+    private fun getInsertMovieInfoQuery(title:String, plot: String, imageUrl: String): String =
+            "insert into info values(null," +
+                    " '${title.replaceQuotes()}'," +
+                    " '${plot.replaceQuotes()}'," +
+                    " '$imageUrl'," +
+                    " 1)"
+
     private fun insertMovieInfo(title:String, plot: String, imageUrl: String){
         try {
             statement?.executeUpdate(getInsertMovieInfoQuery(title, plot,imageUrl))
@@ -143,10 +86,43 @@ object DataBase {
         }
     }
 
-    private fun getInsertMovieInfoQuery(title:String, plot: String, imageUrl: String): String =
-            "insert into info values(null," +
-                    " '${title.replaceQuotes()}'," +
-                    " '${plot.replaceQuotes()}'," +
-                    " '$imageUrl'," +
-                    " 1)"
+    @JvmStatic
+    fun saveMovieInfo(title: String, plot: String, imageUrl: String) {
+        openConnectionToExtraInfo()
+        insertMovieInfo(title,plot,imageUrl)
+        closeConnectionToExtraInfo()
+    }
+
+    private fun getInfoMovieByTitleQuery(title: String): String =
+            ("select * from info WHERE title = '$title'")
+
+    private fun getMovieColumn(title: String, column: String): String? {
+        var plot: String? = null
+        try {
+            val movieInfoResultSet = statement?.executeQuery(getInfoMovieByTitleQuery(title))
+            movieInfoResultSet?.let {
+                if(it.next())
+                    plot = it.getString(column)
+            }
+        } catch (e: SQLException) {
+            System.err.println("Get movie plot error " + e.message)
+        }
+        return plot
+    }
+
+    @JvmStatic
+    fun getOverview(title: String): String? {
+        openConnectionToExtraInfo()
+        val overview = getMovieColumn(title, plotColumn)
+        closeConnectionToExtraInfo()
+        return overview
+    }
+
+    @JvmStatic
+    fun getImageUrl(title: String): String? {
+        openConnectionToExtraInfo()
+        val imageUrl = getMovieColumn(title, imageUrlColumn)
+        closeConnectionToExtraInfo()
+        return imageUrl
+    }
 }
