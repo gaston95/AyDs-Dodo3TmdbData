8cb8f2bba06c6839f1737d908cef9bc0530f46c5
tscandalitta <tscandalitta@gmail.com>
2020-05-29 20:24:37 -0300

    Convertimos el archivo de java a kotlin

--- src/main/java/ayds/dodo/movieinfo/moredetails/fulllogic/OtherInfoWindow.kt
+++ src/main/java/ayds/dodo/movieinfo/moredetails/fulllogic/OtherInfoWindow.kt
@@ -1,231 +1,210 @@
-package ayds.dodo.movieinfo.moredetails.fulllogic;
+package ayds.dodo.movieinfo.moredetails.fulllogic
 
-import ayds.dodo.movieinfo.home.model.entities.OmdbMovie;
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.scalars.ScalarsConverterFactory;
+import ayds.dodo.movieinfo.home.model.entities.OmdbMovie
+import ayds.dodo.movieinfo.moredetails.fulllogic.DataBase.createNewDatabase
+import ayds.dodo.movieinfo.moredetails.fulllogic.DataBase.getImageUrl
+import ayds.dodo.movieinfo.moredetails.fulllogic.DataBase.getOverview
+import ayds.dodo.movieinfo.moredetails.fulllogic.DataBase.saveMovieInfo
+import com.google.gson.Gson
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.awt.BorderLayout
+import java.awt.Desktop
+import java.awt.Dimension
+import java.io.IOException
+import java.net.URL
+import javax.imageio.ImageIO
+import javax.swing.*
+import javax.swing.event.HyperlinkEvent
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import javax.swing.event.HyperlinkEvent;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.net.URL;
-import java.util.Iterator;
+class OtherInfoWindow {
+    private var contentPane: JPanel? = null
+    private var descriptionTextPane: JTextPane? = null
+    private var imagePanel: JPanel? = null
 
-public class OtherInfoWindow {
-  private JPanel contentPane;
-  private JTextPane descriptionTextPane;
-  private JPanel imagePanel;
 
-  private static final String single_line_break = "\n";
-  private static final String local_movie = "[*]";
-  private static final String api_url = "https://api.themoviedb.org/3/";
+    private val path_default = "https://www.themoviedb.org/assets/2/v4/logos/" +
+            "256x256-dark-bg-01a111196ed89d59b90c31440b0f77523e9d9a9acac04a7bac00c27c6ce511a9.png"
+    private val path_url = "https://image.tmdb.org/t/p/w400/"
+    private val link_open = "<a href="
+    private val link_close = "</a>"
+    private val hyperlink_text = "View Movie Poster"
 
-  public static void open(OmdbMovie movie) {
-    OtherInfoWindow win = createWindow();
+    fun getMoviePlot(movie: OmdbMovie) {
+        setHyperLinkListener()
 
-    win.getMoviePlot(movie);
-  }
+        Thread(Runnable {
 
-  private static final String content_type = "text/html";
-  private static final String frame_title = "Movie Info Dodo";
-  private static final String label_text = "Data from The Movie Data Base";
-  private static final int width = 600;
-  private static final int height = 400;
+            createNewDatabase()
+            var text = getOverview(movie.title)
+            var path = getImageUrl(movie.title)
+            if (movieExistsInDb(text, path)) {
+                text = getTextInDB(text)
+            } else {
+                text = "No Results"
+                path = path_default
+                val searchResult = searchMovie(movie)
+                if (searchResult != null) {
+                    val extract = searchResult["overview"]
+                    if (isNotNull(extract)) {
+                        text = extract.asString.replace("\\n", single_line_break)
+                        text = textToHtml(text, movie.title)
+                        val backdropPathJson = searchResult["backdrop_path"]
+                        if (isNotNull(backdropPathJson)) path = path_url + backdropPathJson.asString
+                        val posterPath = searchResult["poster_path"]
+                        if (isNotNull(posterPath)) text += single_line_break + link_open + path_url + posterPath.asString +
+                                ">" + hyperlink_text + link_close
+                        saveMovieInfo(movie.title, text, path)
+                    }
+                }
+            }
+            descriptionTextPane!!.text = text
+            setImage(path)
+        }).start()
+    }
 
-  private static OtherInfoWindow createWindow() {
-    OtherInfoWindow win = new OtherInfoWindow();
-
-    win.contentPane = new JPanel();
-    win.contentPane.setLayout(new BoxLayout(win.contentPane, BoxLayout.PAGE_AXIS));
-    win.contentPane.add(new JLabel(label_text));
-
-    win.imagePanel = new JPanel();
-    win.contentPane.add(win.imagePanel);
-
-    JPanel descriptionPanel = new JPanel(new BorderLayout());
-    win.descriptionTextPane = new JTextPane();
-    win.descriptionTextPane.setEditable(false);
-    win.descriptionTextPane.setContentType(content_type);
-    win.descriptionTextPane.setMaximumSize(new Dimension(width, height));
-    descriptionPanel.add(win.descriptionTextPane);
-    win.contentPane.add(descriptionPanel);
-
-    JFrame frame = new JFrame(frame_title);
-    frame.setMinimumSize(new Dimension(width, height));
-    frame.setContentPane(win.contentPane);
-    frame.pack();
-    frame.setVisible(true);
-
-    return win;
-  }
-
-  private static final String path_default = "https://www.themoviedb.org/assets/2/v4/logos/" +
-          "256x256-dark-bg-01a111196ed89d59b90c31440b0f77523e9d9a9acac04a7bac00c27c6ce511a9.png";
-  private static final String path_url = "https://image.tmdb.org/t/p/w400/";
-  private static final String link_open = "<a href=";
-  private static final String link_close = "</a>";
-  private static final String hyperlink_text = "View Movie Poster";
-
-  public void getMoviePlot(OmdbMovie movie) {
-    setHyperLinkListener();
-
-    new Thread(() -> {
-
-      DataBase.createNewDatabase();
-      String text = DataBase.getOverview(movie.getTitle());
-      String path = DataBase.getImageUrl(movie.getTitle());
-
-      if (movieExistsInDb(text,path)) {
-        text = getTextInDB(text);
-      } else {
-
-        text = "No Results";
-        path = path_default;
-
-        JsonObject searchResult = searchMovie(movie);
-        if(searchResult != null){
-
-          JsonElement extract = searchResult.get("overview");
-          if (isNotNull(extract)) {
-            text = extract.getAsString().replace("\\n", single_line_break);
-            text = textToHtml(text, movie.getTitle());
-
-            JsonElement backdropPathJson = searchResult.get("backdrop_path");
-            if (isNotNull(backdropPathJson))
-              path = path_url + backdropPathJson.getAsString();
-
-            JsonElement posterPath = searchResult.get("poster_path");
-            if(isNotNull(posterPath))
-              text += single_line_break + link_open + path_url + posterPath.getAsString() +
-                      ">" + hyperlink_text + link_close;
-
-            DataBase.saveMovieInfo(movie.getTitle(), text, path);
-          }
+    private fun setHyperLinkListener() {
+        descriptionTextPane!!.addHyperlinkListener { e: HyperlinkEvent ->
+            if (HyperlinkEvent.EventType.ACTIVATED == e.eventType) {
+                val desktop = Desktop.getDesktop()
+                try {
+                    desktop.browse(e.url.toURI())
+                } catch (ex: Exception) {
+                    ex.printStackTrace()
+                }
+            }
         }
-      }
-      descriptionTextPane.setText(text);
-      setImage(path);
-    }).start();
-  }
+    }
 
-  private void setHyperLinkListener(){
-    descriptionTextPane.addHyperlinkListener(e -> {
-      if (HyperlinkEvent.EventType.ACTIVATED.equals(e.getEventType())) {
-        Desktop desktop = Desktop.getDesktop();
+    private fun movieExistsInDb(text: String?, path: String?): Boolean {
+        return text != null && path != null
+    }
+
+    private fun isNotNull(element: JsonElement?): Boolean {
+        return element != null && !element.isJsonNull
+    }
+
+    private fun searchMovie(movie: OmdbMovie): JsonObject? {
+        val tmdbAPI = createAPI()
+        var callResponse: Response<String?>?
         try {
-          desktop.browse(e.getURL().toURI());
-        } catch (Exception ex) {
-          ex.printStackTrace();
+            callResponse = tmdbAPI.getTerm(movie.title)?.execute()
+            val gson = Gson()
+            val jobj = gson.fromJson(callResponse?.body(), JsonObject::class.java)
+            val resultIterator: Iterator<JsonElement> = jobj["results"].asJsonArray.iterator()
+            var result: JsonObject
+            while (resultIterator.hasNext()) {
+                result = resultIterator.next().asJsonObject
+                if (areSameYear(result, movie.year)) {
+                    return result
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
-      }
-    });
-  }
+        return null
+    }
 
-  private boolean movieExistsInDb(String text, String path) {
-    return text != null && path != null;
-  }
+    private fun createAPI(): TheMovieDBAPI {
+        val retrofit = Retrofit.Builder()
+                .baseUrl(api_url)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build()
+        return retrofit.create(TheMovieDBAPI::class.java)
+    }
 
-  private static String getTextInDB(String text) {
-    return local_movie + text;
-  }
+    private fun areSameYear(result: JsonObject, movieYear: String): Boolean {
+        val yearJson = result["release_date"]
+        val year = yearJson?.asString?.split("-")?.toTypedArray()?.get(0) ?: ""
+        return year == movieYear
+    }
 
-  private boolean isNotNull(JsonElement element) {
-    return element != null && !element.isJsonNull();
-  }
-
-  private JsonObject searchMovie(OmdbMovie movie) {
-    TheMovieDBAPI tmdbAPI = createAPI();
-    Response<String> callResponse;
-    try {
-      callResponse = tmdbAPI.getTerm(movie.getTitle()).execute();
-
-      Gson gson = new Gson();
-      JsonObject jobj = gson.fromJson(callResponse.body(), JsonObject.class);
-
-      Iterator<JsonElement> resultIterator = jobj.get("results").getAsJsonArray().iterator();
-      JsonObject result;
-
-      while (resultIterator.hasNext()) {
-        result = resultIterator.next().getAsJsonObject();
-
-        if (areSameYear(result,movie.getYear())) {
-          return result;
+    private fun setImage(path: String?) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+        } catch (ignored: ClassNotFoundException) {
+        } catch (ignored: InstantiationException) {
+        } catch (ignored: IllegalAccessException) {
+        } catch (ignored: UnsupportedLookAndFeelException) {
         }
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+        try {
+            val url = URL(path)
+            val image = ImageIO.read(url)
+            val label = JLabel(ImageIcon(image))
+            imagePanel!!.add(label)
+            contentPane!!.validate()
+            contentPane!!.repaint()
+        } catch (exp: Exception) {
+            exp.printStackTrace()
+        }
     }
-    return null;
-  }
 
-  private TheMovieDBAPI createAPI(){
-    Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl(api_url)
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .build();
+    companion object {
+        private const val single_line_break = "\n"
+        private const val local_movie = "[*]"
+        private const val api_url = "https://api.themoviedb.org/3/"
+        fun open(movie: OmdbMovie) {
+            val win = createWindow()
+            win.getMoviePlot(movie)
+        }
 
-    return retrofit.create(TheMovieDBAPI.class);
-  }
+        private const val content_type = "text/html"
+        private const val frame_title = "Movie Info Dodo"
+        private const val label_text = "Data from The Movie Data Base"
+        private const val width = 600
+        private const val height = 400
+        private fun createWindow(): OtherInfoWindow {
+            val win = OtherInfoWindow()
+            win.contentPane = JPanel()
+            win.contentPane!!.layout = BoxLayout(win.contentPane, BoxLayout.PAGE_AXIS)
+            win.contentPane!!.add(JLabel(label_text))
+            win.imagePanel = JPanel()
+            win.contentPane!!.add(win.imagePanel)
+            val descriptionPanel = JPanel(BorderLayout())
+            win.descriptionTextPane = JTextPane()
+            win.descriptionTextPane!!.isEditable = false
+            win.descriptionTextPane!!.contentType = content_type
+            win.descriptionTextPane!!.maximumSize = Dimension(width, height)
+            descriptionPanel.add(win.descriptionTextPane)
+            win.contentPane!!.add(descriptionPanel)
+            val frame = JFrame(frame_title)
+            frame.minimumSize = Dimension(width, height)
+            frame.contentPane = win.contentPane
+            frame.pack()
+            frame.isVisible = true
+            return win
+        }
 
-  private boolean areSameYear(JsonObject result, String movieYear) {
-    JsonElement yearJson = result.get("release_date");
-    String year = yearJson == null ? "" : yearJson.getAsString().split("-")[0];
 
-    return year.equals(movieYear);
-  }
+        private fun getTextInDB(text: String?): String {
+            return local_movie + text
+        }
 
-  private void setImage(String path) {
-    try {
-      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
-            UnsupportedLookAndFeelException ignored) {
+        private const val html_open = "<html>"
+        private const val body_open = "<body style=\"width: 400px\">"
+        private const val font_open = "<font face=\"arial\">"
+        private const val font_close = "</font>"
+        private const val bold_open = "<b>"
+        private const val bold_close = "</b>"
+        private fun textToHtml(text: String, term: String): String {
+            val builder = StringBuilder()
+            builder.append(html_open + body_open)
+                    .append(font_open)
+            val textWithReplacedQuotes = replaceQuotes(text)
+            builder.append(makeTermBold(textWithReplacedQuotes, term))
+                    .append(font_close)
+            return builder.toString()
+        }
+
+        private fun replaceQuotes(text: String): String {
+            return text.replace("'", "`")
+        }
+
+        private fun makeTermBold(text: String, term: String): String {
+            return text.replace("(?i)" + term.toRegex(), bold_open + term.toUpperCase() + bold_close)
+        }
     }
-    try {
-      URL url = new URL(path);
-      BufferedImage image = ImageIO.read(url);
-      JLabel label = new JLabel(new ImageIcon(image));
-      imagePanel.add(label);
-
-      contentPane.validate();
-      contentPane.repaint();
-
-    } catch (Exception exp) {
-      exp.printStackTrace();
-    }
-  }
-
-  private static final String html_open = "<html>";
-  private static final String body_open = "<body style=\"width: 400px\">";
-  private static final String font_open = "<font face=\"arial\">";
-  private static final String font_close = "</font>";
-  private static final String bold_open = "<b>";
-  private static final String bold_close = "</b>";
-
-  private static String textToHtml(String text, String term) {
-
-    StringBuilder builder = new StringBuilder();
-    builder.append(html_open + body_open)
-            .append(font_open);
-
-    String textWithReplacedQuotes = replaceQuotes(text);
-
-    builder.append(makeTermBold(textWithReplacedQuotes,term))
-            .append(font_close);
-
-    return builder.toString();
-  }
-
-  private static String replaceQuotes(String text){
-    return text.replace("'", "`");
-  }
-
-  private static String makeTermBold(String text, String term) {
-    return text.replaceAll("(?i)" + term, bold_open + term.toUpperCase() + bold_close);
-  }
 }
