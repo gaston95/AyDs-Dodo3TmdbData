5241a7403f0c3783d17e1a272776c121a732184e
gaston <reyesgaston_95@yahoo.com.ar>
2020-06-14 13:34:32 -0700

    MoreDetailsModel
    Finalizamos MoreDetailsModel

--- src/main/java/ayds/dodo/movieinfo/moredetails/model/MoreDetailsModelImp.kt
+++ src/main/java/ayds/dodo/movieinfo/moredetails/model/MoreDetailsModelImp.kt
@@ -1,2 +1,16 @@
 package ayds.dodo.movieinfo.moredetails.model
 
+import ayds.dodo.movieinfo.moredetails.model.entities.TMDBMovie
+import ayds.dodo.movieinfo.moredetails.model.repository.TMDBRepository
+
+interface MoreDetailsModel{
+
+    fun searchMovie(title: String, year: String) : TMDBMovie
+
+}
+
+internal class MoreDetailsModelImpl(private val repository: TMDBRepository) : MoreDetailsModel {
+
+    override fun searchMovie(title: String, year: String) =
+         repository.getMovie(title, year)
+}
--- src/main/java/ayds/dodo/movieinfo/moredetails/model/MoreDetailsModelModule.kt
+++ src/main/java/ayds/dodo/movieinfo/moredetails/model/MoreDetailsModelModule.kt
@@ -1,4 +1,33 @@
 package ayds.dodo.movieinfo.moredetails.model
 
+
+
+
+import ayds.dodo.movieinfo.moredetails.model.repository.TMDBRepositoryImp
+import ayds.dodo.movieinfo.moredetails.model.repository.external.tmdb.TMDBMovieResolverImp
+import ayds.dodo.movieinfo.moredetails.model.repository.external.tmdb.TMDBService
+import ayds.dodo.movieinfo.moredetails.model.repository.external.tmdb.TheMovieDBAPI
+import ayds.dodo.movieinfo.moredetails.model.repository.local.sqldb.SQLQueriesImp
+import ayds.dodo.movieinfo.moredetails.model.repository.local.sqldb.SqlDBImp
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
 object MoreDetailsModelModule {
+
+
+    private val apiUrl = "https://api.themoviedb.org/3/"
+
+    private val retrofit = Retrofit.Builder()
+                .baseUrl(apiUrl)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build()
+
+    private fun getTMDBAPI(): TheMovieDBAPI = retrofit.create(TheMovieDBAPI::class.java)
+
+    private val repository = TMDBRepositoryImp(
+            SqlDBImp(SQLQueriesImp()), TMDBService(getTMDBAPI(),
+            TMDBMovieResolverImp()
+    ))
+
+    val MoreDetailsModel: MoreDetailsModel = MoreDetailsModelImpl(repository)
 }
