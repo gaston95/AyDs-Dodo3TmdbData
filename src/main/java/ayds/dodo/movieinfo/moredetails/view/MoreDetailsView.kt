bc8b895183e40918cad25b666fdfdc3f1e040c5f
tomasZarate <31145950+tomasZarate@users.noreply.github.com>
2020-06-16 10:33:08 -0300

    suscribe observer en controlller

--- src/main/java/ayds/dodo/movieinfo/moredetails/view/MoreDetailsView.kt
+++ src/main/java/ayds/dodo/movieinfo/moredetails/view/MoreDetailsView.kt
@@ -1,10 +1,9 @@
 package ayds.dodo.movieinfo.moredetails.view
 
-import ayds.dodo.movieinfo.home.view.UiEvent
 import ayds.dodo.movieinfo.moredetails.model.entities.TMDBMovie
 import ayds.observer.Observable
 
 interface MoreDetailsView {
     fun openView(movieData: TMDBMovie)
-
+    fun onUiEvent(): Observable<UiEvent>
 }
