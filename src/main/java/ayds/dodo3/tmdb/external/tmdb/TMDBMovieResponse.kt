d1478a043986a56a615700b2344e8efe495b3692
gaston reyes <reyesgaston_95@yahoo.com.ar>
2020-06-29 13:04:00 -0300

    agrego la clase TMDBMovieResponse

---
+++ src/main/java/ayds/dodo3/tmdb/external/tmdb/TMDBMovieResponse.kt
@@ -0,0 +1,16 @@
+package ayds.dodo.movieinfo.moredetails.model.entities
+
+open class TMDBMovie {
+    open var title = ""
+    open var plot = ""
+    open var imageUrl = ""
+    open var posterPath: String? = null
+    open var isLocallyStoraged = false
+}
+
+object DefaultMovie: TMDBMovie() {
+    override var title = "No results"
+    override var plot = "No results"
+    override var imageUrl = "https://www.themoviedb.org/assets/2/v4/logos/" +
+    "256x256-dark-bg-01a111196ed89d59b90c31440b0f77523e9d9a9acac04a7bac00c27c6ce511a9.png"
+}
